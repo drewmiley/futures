89ef426dfdd8a436ec0e04ddfb297eb24d2027f8
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2022-08-01 00:06:36 +0100

    Implements getUserDetailsWithFor

--- src/main/scala/controllers/userdetails/UserDetailsController.scala
+++ src/main/scala/controllers/userdetails/UserDetailsController.scala
@@ -14,7 +14,16 @@ object UserDetailsController {
   // Return formatted String with "<name> - <job role> - <salary>
 
   //TODO implement using maps (Not for yield)
-  def getUserDetailsWithMap: Future[String] = ???
+  def getUserDetailsWithMap: Future[String] = {
+    Future.successful("NOT YET")
+  }
 
-  def getUserDetailsWithFor: Future[String] = ???
+  def getUserDetailsWithFor: Future[String] = {
+    val id: String = "id"
+    for {
+      jobRole <- connector.getJobRole(id)
+      name <- connector.getName(id)
+      salary <- connector.getSalary(name)
+    } yield List(name, jobRole, salary.toString).mkString(" - ")
+  }
 }
