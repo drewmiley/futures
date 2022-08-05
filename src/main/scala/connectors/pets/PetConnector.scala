9aa9c666ce04729b91504fdf69061f2cf6c20b9c
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2022-08-05 01:38:34 +0100

    Removes dummy implementation in connector

--- src/main/scala/connectors/pets/PetConnector.scala
+++ src/main/scala/connectors/pets/PetConnector.scala
@@ -5,10 +5,10 @@ import models.pets.{Dog, Pet}
 import scala.concurrent.Future
 
 trait PetConnector {
-  def getPet(id: String): Future[Pet] = Future.successful(Pet("hello", Dog))
+  def getPet(id: String): Future[Pet] = ???
 
   @throws[PetNotFoundException]("Pet not found")
-  def getPrice(pet: Pet): Future[Double] = Future.successful(1)
+  def getPrice(pet: Pet): Future[Double] = ???
 }
 
 sealed class PetNotFoundException extends Exception("Pet not found")
